4245110ace95315d63ed3d0ca3c0f0da7413b1d3
elcharly <ninguno@elcharly.com>
2018-10-04 13:03:54 -0300

    ed

--- test/restaurant.js
+++ test/restaurant.js
@@ -2,3 +2,16 @@
 // reservarHorario(horario): dado un horario (string) lo busca dentro del arreglo del horarios del restaurante y lo elimina.
 // obtenerPuntuacion(): obtiene la puntuación del restaurante, que es el promedio de todas las calificaciones que recibió. Esta función suma todas las calificaciones que se encuentran en el arreglo de calificaciones y saca el promedio.
 // calificar(nuevaCalificacion): agrega una nueva calificación al arreglo de calificaciones.
+
+var expect = chai.expect;
+
+describe("Reservar Horario", function(){
+  // it("debe agregar un invitado valido a la lista", function(){
+  //   var cantidadAnterior = Invitados.obtenerLista().length;
+  //   Invitados.agregarInvitado("Diego");
+  //   var listaDeInvitados = Invitados.obtenerLista()
+  //   expect(listaDeInvitados.length).to.equal(cantidadAnterior + 1);
+  //   expect(listaDeInvitados[listaDeInvitados.length - 1]).to.equal("Diego");
+  // })
+
+})
