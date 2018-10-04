2a6e875c5651e9c6ed5f4d0e3125e92b7cc1bd23
elcharly <ninguno@elcharly.com>
2018-10-04 11:40:44 -0300

    I was working in the skeleton of the tdd

---
+++ test/listado.js
@@ -0,0 +1,6 @@
+// Clase Listado
+// buscarRestaurante(id): dado el id de un restaurante, devuelve el restaurante del listado que posee ese id.
+// obtC(): obtiene (sin repetidos) todos las ciudades en las cuáles hay un restaurante.
+// obtR(): obtiene (sin repetidos) todos los rubros de los restaurantes.
+// obtH(): obtiene (sin repetidos) todos los posibles horarios de reserva de los restaurantes.
+// obtenerRestaurantes(filtroRubro, filtroCiudad, filtroHorario): función que se encarga de filtrar los restaurantes según los filtros recibidos.
---
+++ test/restaurant.js
@@ -0,0 +1,4 @@
+// Clase Restaurante
+// reservarHorario(horario): dado un horario (string) lo busca dentro del arreglo del horarios del restaurante y lo elimina.
+// obtenerPuntuacion(): obtiene la puntuación del restaurante, que es el promedio de todas las calificaciones que recibió. Esta función suma todas las calificaciones que se encuentran en el arreglo de calificaciones y saca el promedio.
+// calificar(nuevaCalificacion): agrega una nueva calificación al arreglo de calificaciones.
