16f4446fbee28cca5736568f4bab2687a326a5ff
elcharly <ninguno@elcharly.com>
2018-10-05 12:49:33 -0300

    ed

--- test/restaurant.js
+++ test/restaurant.js
@@ -9,19 +9,11 @@ describe("Clase Restaurant", function(){
   beforeEach(function(){
     restaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
   })
-  // afterEach(function(){
-  //   console.log("Wow");
-  // })
-  describe("Reservar Horario", function(){
-
-    // it("debe agregar un invitado valido a la lista", function(){
-    //   var cantidadAnterior = Invitados.obtenerLista().length;
-    //   Invitados.agregarInvitado("Diego");
-    //   var listaDeInvitados = Invitados.obtenerLista()
-    //   expect(listaDeInvitados.length).to.equal(cantidadAnterior + 1);
-    //   expect(listaDeInvitados[listaDeInvitados.length - 1]).to.equal("Diego");
-    // })
-    it("cuando se reserva horario, este se debe eliminar del arreglo",function(){
+  describe("Funcion reservarHorario", function(){
+    // la cantidad de elementos del arreglo disminuya o no según corresponda.
+    // el arreglo se mantenga igual, exactamente con los mismos elementos.
+    // el arreglo cambie y contenga exactamente los elementos que corresponden.
+    it("Cuando se reserva un horario de un restaurant, el horario correspondiente se elimina del arreglo.",function(){
       var cantidadDeElementos = restaurant.horarios.length;
       console.log(cantidadDeElementos);
       restaurant.reservarHorario("15:30");
@@ -30,29 +22,29 @@ describe("Clase Restaurant", function(){
       //la cantidad de elementos del arreglo disminuya o no según corresponda
       var cantidadDeElementos2 = restaurant.horarios.length;
       expect(cantidadDeElementos2).to.equal(cantidadDeElementos-1);
-
     })
-
-    it("cuando se reserva horario que no esta, el arreglo queda igual",function(){
+    it("Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.",function(){
       restaurant.reservarHorario("15:70");
       expect(restaurant.horarios).to.eql(["13:00", "15:30", "18:00"]);
     })
-
-    it("cuando se intenta reservar un horario sin pasar parametro, el arreglo queda igual",function(){
+    it("Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.",function(){
       restaurant.reservarHorario();
       expect(restaurant.horarios).to.eql(["13:00", "15:30", "18:00"]);
-
     })
   })
-
-  describe("Obtener Puntuacion", function(){
-
-  it("Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.",function(){
+  describe("Funcion obtenerPuntuacion", function(){
+    it("Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.",function(){
       var puntuacion = restaurant.obtenerPuntuacion();
       //expect(restaurant.horarios).to.exist("15:30");
       expect(puntuacion).to.equal(7.4);
     })
 
+    it("Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.",function(){
+        // var puntuacion = restaurant.obtenerPuntuacion();
+        // //expect(restaurant.horarios).to.exist("15:30");
+        // expect(puntuacion).to.equal(7.4);
+    })
+    
   // it("Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.",function(){
   //     var puntuacion = restaurant.obtenerPuntuacion();
   //     //expect(restaurant.horarios).to.exist("15:30");
